e58f814addcabf2f6126b73057b804f1031b41d0
Luana_Thurow <luana.thurow@dell.com>
2025-12-11 15:30:18 -0300

    Structure the site's HTML: Add basic data.

--- portfolio/src/app/modules/portfolio/pages/home/home.component.ts
+++ portfolio/src/app/modules/portfolio/pages/home/home.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
+//Components
+import { HeaderComponent } from '../../components/header/header.component';
+
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
