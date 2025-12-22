482233984b47f0071340de4065258c8e445124d5
Luana_Thurow <luana.thurow@dell.com>
2025-12-22 16:41:27 -0300

    Reorder icons

--- portfolio/src/app/modules/portfolio/components/knowledge/knowledge.component.ts
+++ portfolio/src/app/modules/portfolio/components/knowledge/knowledge.component.ts
@@ -9,15 +9,14 @@ import { IKnowledge } from '../../interface/Iknowledge.interface';
   templateUrl: './knowledge.component.html',
   styleUrl: './knowledge.component.scss'
 })
+
 export class KnowledgeComponent {
   public arrayKnowledge = signal<IKnowledge[]>([
-    {
-      src: 'assets/knowledge/csharp.svg',
-      alt: 'Ícone de conhecimento C#',
-    },
-    {
-      src: 'assets/knowledge/go.svg',
-      alt: 'Ícone de conhecimento Go',
-    },
+    { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg', alt: 'C#' },
+    { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/angular/angular-original.svg', alt: 'Angular' },
+    { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/azuresqldatabase/azuresqldatabase-original.svg', alt: 'Sql' },
+    { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/gitlab/gitlab-original.svg', alt: 'GitLab' },
+    { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/amazonwebservices/amazonwebservices-plain-wordmark.svg', alt: 'Aws' },
+    { src: 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/docker/docker-original.svg', alt: 'Docker' },
   ]);
 }
